5287001c7d0f2312ed0e608f0984f797a3e84172
Abhishek Sharma <abhisheksharma532@gmail.com>
2019-02-13 00:22:30 +0530

    Fixed bug where denominator was zero

--- software.py
+++ software.py
@@ -1,7 +1,11 @@
 import math
+import sys
 
 def add(a,b):
     return a + b
 
 def divide(a,b):
+    if b == 0:
+        print('Denominator cannot be zero! Exiting!')
+        sys.exit(-1)
     return a / b
